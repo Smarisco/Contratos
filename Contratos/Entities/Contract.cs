6dbd7e39271d1cd15d5ffaeb39ecb7a9d94574b2
Stela Marisco <smarisco@yahoo.com.br>
2021-01-08 09:13:14 -0400

    Adicionar arquivos de projeto.

---
+++ Contratos/Entities/Contract.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Contratos.Entities
+{
+    class Contract
+    {
+        public int number { get; set; }
+        public DateTime date { get; set; }
+        public double totalValue { get; set; }
+        public List<Installment> Installments { get; set; }
+
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            this.number = number;
+            this.date = date;
+            this.totalValue = totalValue;
+            Installments = new List<Installment>();
+        }
+        public void AddInstallment(Installment installment)
+        {
+            Installments.Add(installment);
+        }
+    }
+}
